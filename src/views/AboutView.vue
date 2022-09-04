320dd00bf39f4bc850ed1aba7d643f5d60baf130
Srimanta Pal <srimantapal205@hotmail.com>
2022-09-04 10:42:37 +0530

    Practice vue code

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button type="button" @click="getData('Function Called')">SingleClick Button</button>
+    <button type="button" @dblclick="getData('Function Called')">Double Click Button</button>
+    <h2 @mousemove="getConsole()" >Detected Mouse move event</h2>
+    <h2>{{changeValue}}</h2>
+    <button @click="getIncrementValue">Increment Value</button>
+    <button @click="getDicrementValue">Dicrement Value</button>
+    <h2>Get Input fields value</h2>
+    <input type="text" placeholder="Enter email" v-model="email">
+    <br>
+    <br>
+    <input type="password" placeholder="Enter Password" v-model="pass">
+    <br><br>
+    <button type="button" @click="getDataValue">Get Data</button>
+    
   </div>
 </template>
+
+<script>
+
+export default {
+  name: "aboutPage",
+  data() {
+    return {
+      changeValue: 0,
+      email:null,
+      pass:null
+    }
+  },
+  methods: {
+    getDataValue(){
+      console.warn("Values :", this.email, this.pass)
+    },
+
+    getData(x) {
+      alert(x);
+    },
+    getConsole(){
+      console.log("mouse is moving")
+    },
+    getIncrementValue(){
+        return this.changeValue++
+    },
+    getDicrementValue(){
+      return this.changeValue--
+    }
+
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,15 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{email}}</h2>
+    <P>{{getName('Javascript')}}</P>
+    <p>{{getData()}}</p>
+    <p>{{getData().name}}</p>
+    <p>{{getData().email}}</p>
   </div>
+  <h2> two way binding</h2>
+  <input type="text" name="incValue" id="inc" v-model="count">
+  <h3>{{count}}</h3>
 </template>
 
 <script>
@@ -13,6 +21,24 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      email: "test@test.com",
+      count:110
+    }
+  },
+  methods: {
+    getName(x) {
+      return x
+    },
+    getData(){
+      return {
+        name:"This is return Object",
+        email:"return@test.in", 
+        email2:this.email
+      }
+    }
+  },
 }
 </script>
